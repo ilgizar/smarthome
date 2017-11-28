2b227e6ad3e3ad505430a509dbbd672c59b4e995
ilgizar <ilgizar@gmail.com>
2017-11-28 15:56:30 +0500

    Lib smarthome

---
+++ libs/smarthome/smarthome.go
@@ -0,0 +1,8 @@
+package smarthome
+
+var TypesOfDays = []string {
+    "leave",            // work vacation
+    "vacation",         // school vacation
+    "holiday",          // holiday
+    "workday",          // work day
+}
